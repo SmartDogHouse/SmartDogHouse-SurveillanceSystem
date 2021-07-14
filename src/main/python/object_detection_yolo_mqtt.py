947c75f909f451068ec25a483a391904464cc951
igor-lirussi <igor.lirussi@studio.unibo.it>
2021-07-14 17:52:01 +0200

    union obj_detection + mqtt

---
+++ src/main/python/object_detection_yolo_mqtt.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import argparse
+import cv2
+import os
+import time
+
+from secret import PATH_TO_CERT, PATH_TO_ROOT, PATH_TO_KEY, MQTT_PORT, MQTT_CLIENT_ID, MQTT_HOST, MQTT_RECEIVE_TOPIC, MQTT_NOTIFY_TOPIC
+from mqtt_manager import MQTTManager
+
+
+mqtt_manager = MQTTManager(cert_path=PATH_TO_CERT, key_path=PATH_TO_KEY,  root_path=PATH_TO_ROOT, port=MQTT_PORT, client_id=MQTT_CLIENT_ID,
+                           server=MQTT_HOST)
+
+
+try:
+    mqtt_manager.connect()
+except Exception as e:
+    print('Cannot connect MQTT: ' + str(e))
+
+def extract_boxes_confidences_classids(outputs, confidence, width, height):
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in outputs:
+        for detection in output:
+            # Extract the scores, classid, and the confidence of the prediction
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            conf = scores[classID]
+
+            # Consider only the predictions that are above the confidence threshold
+            if conf > confidence:
+                # Scale the bounding box back to the size of the image
+                box = detection[0:4] * np.array([width, height, width, height])
+                centerX, centerY, w, h = box.astype('int')
+
+                # Use the center coordinates, width and height to get the coordinates of the top left corner
+                x = int(centerX - (w / 2))
+                y = int(centerY - (h / 2))
+
+                boxes.append([x, y, int(w), int(h)])
+                confidences.append(float(conf))
+                classIDs.append(classID)
+
+    return boxes, confidences, classIDs
+
+
+def draw_bounding_boxes(image, boxes, confidences, classIDs, idxs, colors):
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            # extract bounding box coordinates
+            x, y = boxes[i][0], boxes[i][1]
+            w, h = boxes[i][2], boxes[i][3]
+
+            # draw the bounding box and label on the image
+            color = [int(c) for c in colors[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(labels[classIDs[i]], confidences[i])
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    return image
+
+
+def make_prediction(net, layer_names, labels, image, confidence, threshold):
+    height, width = image.shape[:2]
+
+    # Create a blob and pass it through the model
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(layer_names)
+
+    # Extract bounding boxes, confidences and classIDs
+    boxes, confidences, classIDs = extract_boxes_confidences_classids(outputs, confidence, width, height)
+
+    # Apply Non-Max Suppression
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence, threshold)
+
+    return boxes, confidences, classIDs, idxs
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--weights', type=str, default='model/yolov4-tiny.weights', help='Path to model weights')
+    parser.add_argument('-cfg', '--config', type=str, default='model/yolov4-tiny.cfg', help='Path to configuration file')
+    parser.add_argument('-l', '--labels', type=str, default='model/coco.names', help='Path to label file')
+    parser.add_argument('-c', '--confidence', type=float, default=0.7, help='Minimum confidence for a box to be detected.')
+    parser.add_argument('-t', '--threshold', type=float, default=0.3, help='Threshold for Non-Max Suppression')
+    parser.add_argument('-u', '--use_gpu', default=False, action='store_true', help='Use GPU (OpenCV must be compiled for GPU). For more info checkout: https://www.pyimagesearch.com/2020/02/03/how-to-use-opencvs-dnn-module-with-nvidia-gpus-cuda-and-cudnn/')
+    parser.add_argument('-s', '--save', default=False, action='store_true', help='Whether or not the output should be saved')
+    parser.add_argument('-sh', '--show', default=True, action="store_false", help='Show output')
+
+    input_group = parser.add_mutually_exclusive_group()
+    input_group.add_argument('-i', '--image_path', type=str, default='', help='Path to the image file.')
+    input_group.add_argument('-v', '--video_path', type=str, default='', help='Path to the video file.')
+
+    args = parser.parse_args()
+
+    # Get the labels
+    labels = open(args.labels).read().strip().split('\n')
+
+    # Create a list of colors for the labels
+    colors = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
+
+    # Load weights using OpenCV
+    net = cv2.dnn.readNetFromDarknet(args.config, args.weights)
+
+    if args.use_gpu:
+        print('Using GPU')
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+    if args.save:
+        print('Creating output directory if it doesn\'t already exist')
+        os.makedirs('output', exist_ok=True)
+
+    # Get the output layer names
+    layer_names = net.getLayerNames()
+    layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    if args.image_path != '':
+        # image path specified
+        image = cv2.imread(args.image_path)
+
+        boxes, confidences, classIDs, idxs = make_prediction(net, layer_names, labels, image, args.confidence, args.threshold)
+
+        image = draw_bounding_boxes(image, boxes, confidences, classIDs, idxs, colors)
+
+        # show the output image
+        if args.show:
+            cv2.imshow('YOLO Object Detection', image)
+            cv2.waitKey(0)
+
+        if args.save:
+            cv2.imwrite(f'output/{args.image_path.split("/")[-1]}', image)
+    else:
+        # video path specified
+        if args.video_path != '':
+            cap = cv2.VideoCapture(args.video_path)
+        else:
+            # nothing specified, using camera
+            cap = cv2.VideoCapture(0)
+
+        if args.save:
+            width = int(cap.get(3))
+            height = int(cap.get(4))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            name = args.video_path.split("/")[-1] if args.video_path else 'camera.avi'
+            out = cv2.VideoWriter(f'output/{name}', cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width, height))
+
+        previous_notification_sent = time.time()
+        # while loop for frames
+        while cap.isOpened():
+            ret, image = cap.read()
+
+            if not ret:
+                print('Video file finished.')
+                break
+
+            boxes, confidences, classIDs, idxs = make_prediction(net, layer_names, labels, image, args.confidence, args.threshold)
+            # print("boxes {}, confidences {}, classIDs {}, idxs {}".format(boxes, confidences, classIDs, idxs))
+
+            image = draw_bounding_boxes(image, boxes, confidences, classIDs, idxs, colors)
+
+            # send mqtt message
+            # if detected some id's of classes
+            if len(idxs) > 0:
+                if time.time() - previous_notification_sent > 5:
+                    # sent notify if enough time passed since the last one
+                    previous_notification_sent = time.time()
+                    text = "Security detected: "
+                    for i in idxs.flatten():
+                        text += "{}: {:.2f}".format(labels[classIDs[i]], confidences[i])
+                    # print("DETECTIONS: "+text)
+                    msg = {"Notify": text}
+                    mqtt_manager.send_msg(topic=MQTT_NOTIFY_TOPIC, msg=msg)
+
+
+            if args.show:
+                cv2.imshow('YOLO Object Detection', image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            if args.save:
+                out.write(image)
+
+        cap.release()
+        if args.save:
+            out.release()
+    cv2.destroyAllWindows()
